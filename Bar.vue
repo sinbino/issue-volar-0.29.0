d30260f4c5350a9658f4dd079ba4822a20eb0e17
Fukui Hiroyuki <sinbino@gmail.com>
2021-11-08 10:57:58 +0900

    first commit

---
+++ Bar.vue
@@ -0,0 +1,27 @@
+<!--
+This template will cause the following error in volar v0.29.0
+
+  Extraneous children found when component already has explicitly named default slot. These children will be ignored.
+  ```html
+  <x__VLS_ />
+  ```vue(39)
+  (property) template: any
+  The template element is used to declare fragments of HTML that can be cloned and inserted in the document by script.
+-->
+
+<template lang="pug">
+Foo
+  template(v-slot:header)
+    div FOO
+
+  template(v-slot)
+    div Bar
+</template>
+
+<script>
+import Foo from './Foo.vue'
+
+export default {
+  components: {Foo},
+}
+</script>
---
+++ Foo.vue
@@ -0,0 +1,27 @@
+<!--
+This template will cause the following error in volar v0.29.0
+
+  v-else/v-else-if has no adjacent v-if or v-else-if.vue(30)
+  (property) x__VLS_: any
+-->
+
+<template lang="pug">
+div
+  slot(name="header")
+
+  div.foo(v-if="cond")
+    slot
+
+  div.bar(v-else)
+    slot
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cond: true,
+    }
+  },
+}
+</script>
